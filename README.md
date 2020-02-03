0b5108e20fffe31506c20e4150f67765352e27cb
Ian Porada <iporada@stevens.edu>
2020-02-02 20:45:30 -0500

    Fixed conllu indexing.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # conllu-amod-extract
 Really simple script for extracting (noun, adjective) bigrams from conllu files.
 
-Usage (where `$INPUT_DIR` is a directory of `.conllu` files):
+Usage (where `$INPUT_DIR` is a directory of `.conllu` files; `$OUTPUT_FNAME` is a `.tsv`):
 ```shell script
-python extract_amod.py $INPUT_DIR $OUTPUT_DIR
+python extract_amod.py $INPUT_DIR $OUTPUT_FNAME
 ```
--- extract_amod.py
+++ extract_amod.py
@@ -13,18 +13,16 @@ from conll import CoNLLFile
 def main():
     # get arguments
     parser = argparse.ArgumentParser()
-    # main arguments
     parser.add_argument('input')
     parser.add_argument('output')
-
     args = parser.parse_args()
 
     # build document
     print('Extracting amod bigrams from directory: %s' % args.input)
 
-    for directory, subdirectories, files in os.walk(args.input):
-        amod_bigrams = collections.defaultdict(int)
+    amod_bigrams = collections.defaultdict(int)
 
+    for directory, subdirectories, files in os.walk(args.input):
         for file in files:
             fname = os.path.join(directory, file)
             conll_file = CoNLLFile(fname)
@@ -33,22 +31,21 @@ def main():
             for sent in sents:
                 for i in range(0, len(sent)):
                     line = sent[i]
-                    if line[7] == 'amod' and line[3] == 'ADJ':
+                    if line[7] == 'amod':
                         amod = line[2] # get the lemma
-                        noun_idx = int(line[6]) - 1
+                        noun_idx = int(line[6])
                         noun_line = sent[noun_idx]
-                        if noun_line[3] == 'NOUN':
+                        if noun_line[3] == 'NN' or noun_line[3] == 'NOUN':
                             noun = noun_line[2] # get the lemma
                             amod_bigrams[(amod, noun)] += 1
 
+    print('Writing to output file: %s' % args.output)
 
-        out_fname = os.path.join(args.output, '%s_amod.tsv' % os.path.basename(directory))
-        with open(out_fname, 'w') as csv_file:
-            writer = csv.writer(csv_file, delimiter='\t')
-            for k, v in amod_bigrams.items():
-                amod, noun = k
-                writer.writerow([amod.lower(), noun.lower(), v])
-        print('writing %s' % out_fname)
+    with open(args.output, 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter='\t')
+        for k, v in amod_bigrams.items():
+            amod, noun = k
+            writer.writerow([amod.lower(), noun.lower(), v])
 
     print('done.')
 
